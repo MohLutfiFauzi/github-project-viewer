b2f331d1b5dd08725b72313972ce7ad5fdfcd884
moh.fauzi <moh.fauzi@ist.id>
2025-05-30 06:15:38 +0700

    fix deploy3

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.module.css";
+import "./styles/index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
